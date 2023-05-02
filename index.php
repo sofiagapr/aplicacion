cdd5304460c28c672d18e9a92ef3ab8d1d8be648
sofiagapr <zofiagapr@gmmail.com>
2023-05-02 15:04:07 -0500

    aplicacion

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primera aplicacion</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+  <div class="mb-3">
+    <label for="identificador" class="form-label">Identificador</label>
+    <input type="texto" class="form-control" id="identificador" name="identificador">
+     </div>
+     <div class="mb-3">
+    <label for="nombre" class="form-label">Nombre</label>
+    <input type="texto" class="form-control" id="nombre" name="nombre">
+     </div>
+     <div class="mb-3">
+    <label for="detalle" class="form-label">Detalle</label>
+    <input type="texto" class="form-control" id="detalle" name="detalle">
+     </div>
+  <button type="submit" class="btn btn-primary">Registrar</button>
+</form>
+</body>
+</html>
